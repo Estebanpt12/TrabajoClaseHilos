4e02e55ded3265bdf665681fa0878c61d393e569
Estebanpt12 <esteban.penap@uqvirtual.edu.co>
2022-10-27 17:25:52 -0500

    Implementacion primer main

--- src/App.java
+++ src/App.java
@@ -1,11 +1,26 @@
+import hilos.Hilo1;
 import hilos.Hilo2;
+import hilos.Hilo3;
 import implJoin.ContarConsonantes;
 import implJoin.NumeroFibonacci;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*Hilo2 hilo2 = new Hilo2(2);
-        hilo2.run();*/
+
+        /* Implementacion primeros tres puntos */
+        Hilo1 hilo1 = new Hilo1();
+        hilo1.start();
+        hilo1.join();
+
+        Hilo2 hilo2 = new Hilo2(2);
+        hilo2.run();
+        hilo2.join();
+
+        Hilo3 hilo3 = new Hilo3("Hilo 3!");
+        hilo3.start();
+        hilo3.join();
+
+        /* Implementacion segundo punto (Join) */
         NumeroFibonacci n = new NumeroFibonacci(10);
         n.start();
         ContarConsonantes contarConsonantes = new ContarConsonantes("Hola");
--- src/hilos/Hilo1.java
+++ src/hilos/Hilo1.java
@@ -9,7 +9,7 @@ public class Hilo1 extends Thread{
 
     @Override
     public void run(){
-        for (int i=0; i<=10;i++){
+        for (int i=0; i<arreglo.length;i++){
             System.out.println(arreglo[i]);
             try {
                 sleep(1000);
