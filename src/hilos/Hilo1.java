762b0cb6510b60b0c121759c9d3acbf10109d0a1
Cdrian18 <valenciacarlos1218@gmail.com>
2022-10-27 16:46:20 -0500

    Hilo 1

---
+++ src/hilos/Hilo1.java
@@ -0,0 +1,24 @@
+package hilos;
+
+public class Hilo1 extends Thread{
+
+
+    int[] arreglo = {1,2,3,4,5,6,7,8,9,10};
+
+
+
+    @Override
+    public void run(){
+        for (int i=0; i<=10;i++){
+            System.out.println(arreglo[i]);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+}
